c598b605c45c393a0f78b7b02b095d828ff42c66
willsamc16 <42549415+willsamc16@users.noreply.github.com>
2018-10-25 13:50:37 -0700

    Add files via upload

--- Doodad.java
+++ Doodad.java
@@ -5,6 +5,12 @@ class Doodad
   {
     myNum = 0;
   }
-  public double getNum(){/* your java code */}
-  public void setNum(double n){/* your java code */}
+  public double getNum()
+  {
+    return myNum;
+  }
+  public void setNum(double n)
+  {
+    myNum = n;
+  }
 }
